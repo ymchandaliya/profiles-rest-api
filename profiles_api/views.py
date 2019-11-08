f8e34cb6a5a4612ab3ae3964c49ff7690cf2da81
yash <ymchadaliya45@gmail.com>
2019-11-08 12:39:57 +0530

    ApiView Added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class ProfileAPIView(APIView):
+    '''testing api views'''
+    def get(self, request, format=None):
+        '''testing get method for api '''
+        an_api=[
+            'This is first time I am using api',
+            'Another List Item',
+            'One more added'
+        ]
+        return Response({'Hello':'Hello Api View','an_api':an_api})
